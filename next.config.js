18b00f57724ec53d29cf85fd5541d2f053b546ca
nnwayaye <nnwayaye185@gmail.com>
2026-09-04 12:00:50 +0630

    Fix Server Actions for GitHub Codespaces origins

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+    const nextConfig = {
+    experimental: {
+      serverActions: {
+        allowedOrigins: ["*.app.github.dev", "localhost:3000"],
+      },
+    },
+    };
 
-module.exports = nextConfig;
+    module.exports = nextConfig;
+    
